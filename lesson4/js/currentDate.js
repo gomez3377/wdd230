2d6e232b0ba564ea1701a76af34652c58a5c11dc
gomez3377 <jongomez3377@gmail.com>
2021-10-03 06:02:42 -0600

    c

--- lesson4/js/currentDate.js
+++ lesson4/js/currentDate.js
@@ -1,3 +1,6 @@
+// This JS was based on the codepen by Alesha Anderson McCown
+// See for reference https://codepen.io/aleshana/pen/OJbJovG
+
 const dayNames = [
   "Sunday",
   "Monday",
@@ -7,7 +10,29 @@ const dayNames = [
   "Friday",
   "Saturday",
 ];
-const monthNames [
-    "January"
-]
-const currentDate = new Date(year, month);
+const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+
+];
+
+const currentDate = new Date();
+const currentWeekday = dayNames[currentDate.getDay()];
+const currentDay = currentDate.getDate();
+const currentMonth = monthNames [currentDate.getMonth()];
+const currentYear = currentDate.getFullYear();
+
+const currentFullDate = currentWeekday + ", " + currentDay + " " + currentMonth + " " + currentYear; 
+
+
+document.getElementById("currentDate").innerHTML = currentFullDate;
