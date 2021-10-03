f7df77bd19a016176c7f98f0cd15576ddd76ddb6
gomez3377 <jongomez3377@gmail.com>
2021-10-03 05:24:43 -0600

    sunday

---
+++ lesson4/js/currentDate.js
@@ -0,0 +1,13 @@
+const dayNames = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const monthNames [
+    "January"
+]
+const currentDate = new Date(year, month);
