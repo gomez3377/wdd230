501efe525728b5081ad79f0c0f7969d8306da151
gomez3377 <jongomez3377@gmail.com>
2021-10-21 17:08:26 -0600

    thurs

--- lesson6/js/windChill.js
+++ lesson6/js/windChill.js
@@ -1,18 +1,22 @@
-const currentTemperature = parseFloat(document.querySelector("#currentTemperature").textContent);
+const currentTemperature = parseFloat(
+  document.querySelector("#currentTemperature").textContent
+);
 
-const windSpeed = parseFloat(document.querySelector('#windSpeed').textContent);
+const windSpeed = parseFloat(document.querySelector("#windSpeed").textContent);
 if (currentTemperature <= 50 && windSpeed > 3.0) {
-    
-    const windChill = windChillFunc(currentTemperature,windSpeed);
-}
-else {
-    windChill = "N/A"
+  const windChill = windChillFunc(currentTemperature, windSpeed);
+} else {
+  windChill = "N/A";
 }
 
-function windChillFunc(t,s) {
-    windChill = 35.74 + (0.6215 * t) - (35.75 * Math.pow(s,0.16)) + (0.4275 * t * Math.pow(s,0.16));
-    windChill = Math.round(windChill);
-    return windChill;
+function windChillFunc(t, s) {
+  let f =
+    35.74 +
+    0.6215 * t -
+    35.75 * Math.pow(s, 0.16) +
+    0.4275 * t * Math.pow(s, 0.16);
+  f = Math.round(f);
+  return f;
 }
 
-document.querySelector("#windChill").textContent = windChill
+document.querySelector("#windChill").textContent = windChill;
