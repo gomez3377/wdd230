6628f3f1a980784e52e9acc5e6665fa635043dc1
gomez3377 <jongomez3377@gmail.com>
2021-10-27 17:47:34 -0600

    wednesday

--- lesson7/js/gallery.js
+++ lesson7/js/gallery.js
@@ -49,6 +49,9 @@ function toggleMenu() {
 if (currentWeekday === "Friday") {
 document.getElementById("banner").style.display = "block";
 } 
+
+
+
 // Webfront Load 
 
 WebFont.load ({
@@ -59,3 +62,39 @@ WebFont.load ({
         ]
     }
 });
+
+
+                // Lazy Load Images
+// Lazy Load Function From MDN Progressive Loading Page
+
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+};
+
+
+// The Intersection Observer 
+// Function from MDN Progressive Loading Page
+if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) =>{
+        items.forEach((item) => {
+            if(item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    });
+    
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
+else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
+
