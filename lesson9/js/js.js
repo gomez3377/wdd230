8216cc2efd0ddc241a8e9221c934e1931f9e2d17
gomez3377 <jongomez3377@gmail.com>
2021-11-11 19:00:50 -0700

    thursday

--- lesson9/js/js.js
+++ lesson9/js/js.js
@@ -50,28 +50,67 @@ fetch(requestURL)
     console.table(jsonObject);
     const towns = jsonObject['towns'];
 
-    for (let i = 0; i < 4; i++){
-        let card = document.createElement('section');
-        let townName = document.createElement('h2');
-        let motto = document.createElement('p');
-        let yearFounded = document.createElement('p');
-        let population = document.createElement('p');
-        let rainfall = document.createElement('p');
-   
+    for (let i = 0; i <= 6; i++){
+        if (towns[i].name === "Soda Springs" || towns[i].name === "Fish Haven" || towns[i].name === "Preston") {
 
-        // h2.textContent = towns[i].name + ' ' + prophets[i].lastname;
-        // birthDate.textContent = 'Date of Birth: ' + prophets[i].birthdate;
-        // birthPlaces.textContent = 'Place of Birth: ' + prophets[i].birthplace; 
-        prophetImg.src = prophets[i].imageurl;
-        prophetImg.alt = 'prophetImg';
+            let card = document.createElement('section');
+            let townName = document.createElement('h2');
+            let motto = document.createElement('p');
+            let yearFounded = document.createElement('p');
+            let population = document.createElement('p');
+            let rainfall = document.createElement('p');
+            let townImgContainer = document.createElement('picture');
+            let townImgSource1 = document.createElement('source');
+            let townImgSource2 = document.createElement('source');
+            let townImgSource3 = document.createElement('source');
+            let townImg = document.createElement('img');
+            
+            townName.textContent = towns[i].name;
+            motto.textContent = towns[i].motto
+            yearFounded.textContent = towns[i].yearFounded;
+            population.textContent = towns[i].currentPopulartion;
+            rainfall.textContent = towns[i].averageRainfall;
+            
+            townImgSource1.media = '(max-width: 200px)';
+            townImgSource2.media = '(max-width: 500px)';
+            townImgSource3.media = '(max-width: 800px)';
+            townImg.alt = 'townImg';
 
-        card.appendChild(h2);
-        card.appendChild(birthDate);
-        card.appendChild(birthPlaces);
-        card.appendChild(prophetImg);
-        
-        document.querySelector('div.townCards').appendChild(card);
+            townImgContainer.appendChild(townImg);
+            townImgContainer.appendChild(townImgSource1);
+            townImgContainer.appendChild(townImgSource2);
+            townImgContainer.appendChild(townImgSource3);
+            card.appendChild(townName);
+            card.appendChild(motto);
+            card.appendChild(yearFounded);
+            card.appendChild(population);
+            card.appendChild(rainfall);
+            card.appendChild(townImgContainer);
+
+            if (towns[i].name === "Soda Springs") {
+                townImgSource1.srcset="img/sodaSpringsImg200.jpg";
+                townImgSource2.srcset="img/sodaSpringsImg500.jpg";
+                townImgSource3.srcset="img/sodaSpringsImg800.jpg";
+                townImg.src="img/sodaSpringsImg800.jpg";
+            }
+            if (towns[i].name === "Fish Haven") {
+                townImgSource1.srcset="img/fishHavenImg200.jpg";
+                townImgSource2.srcset="img/fishHavenImg500.jpg";
+                townImgSource3.srcset="img/fishHavenImg800.jpg";
+                townImg.src="img/fishHavenImg800.jpg";
+            }
+            
+            if (towns[i].name === "Preston") {
+                townImgSource1.srcset="img/prestonImg200.jpg";
+                townImgSource2.srcset="img/prestonImg500.jpg";
+                townImgSource3.srcset="img/prestonImg800.jpg";
+                townImg.src="img/prestonImg800.jpg";
+            }
+            document.querySelector('div.townCards').appendChild(card);
+        }
     }
+
+
 });
 
 WebFont.load({
