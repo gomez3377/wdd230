6c6436c9b72928d61566c86f82e667f0e9ce545e
gomez3377 <jongomez3377@gmail.com>
2021-12-15 21:03:42 -0700

    wednesday

--- finalProject/js/directory.js
+++ finalProject/js/directory.js
@@ -7,7 +7,9 @@ fetch(requestURL)
     console.table(jsonObject);
     const business = jsonObject;
 
-    let businessCardsOne = document.createElement('section')
+    let businessCardsOne = document.createElement('li')
+    let businessCardTextContentOne = document.createElement('div')
+    businessCardTextContentOne.className = "directory-text-container"
     let businessNameOne = document.createElement('h3');
     let businessOnwerOne = document.createElement('p');
     let streetAddressOne = document.createElement('p');
@@ -16,17 +18,21 @@ fetch(requestURL)
     let websiteOne = document.createElement('p');
     let businessLogoOne = document.createElement('img');
     
-    let businessCardsTwo = document.createElement('section')
+    let businessCardsTwo = document.createElement('li')
     let businessNameTwo = document.createElement('h3');
+    let businessCardTextContentTwo = document.createElement('div')
+    businessCardTextContentTwo.className = "directory-text-container"
     let businessOnwerTwo = document.createElement('p');
     let streetAddressTwo = document.createElement('p');
     let cityAndStateTwo = document.createElement('p');
     let phoneNumberTwo = document.createElement('p');
     let websiteTwo = document.createElement('p');
     let businessLogoTwo = document.createElement('img');
-   
-    let businessCardsThree = document.createElement('section')
+    
+    let businessCardsThree = document.createElement('li')
     let businessNameThree = document.createElement('h3');
+    let businessCardTextContentThree = document.createElement('div')
+    businessCardTextContentThree.className = "directory-text-container"
     let businessOnwerThree = document.createElement('p');
     let streetAddressThree = document.createElement('p');
     let cityAndStateThree = document.createElement('p');
@@ -34,8 +40,10 @@ fetch(requestURL)
     let websiteThree = document.createElement('p');
     let businessLogoThree = document.createElement('img');
     
-    let businessCardsFour = document.createElement('section')
+    let businessCardsFour = document.createElement('li')
     let businessNameFour = document.createElement('h3');
+    let businessCardTextContentFour = document.createElement('div')
+    businessCardTextContentFour.className = "directory-text-container"
     let businessOnwerFour = document.createElement('p');
     let streetAddressFour = document.createElement('p');
     let cityAndStateFour = document.createElement('p');
@@ -43,8 +51,10 @@ fetch(requestURL)
     let websiteFour = document.createElement('p');
     let businessLogoFour = document.createElement('img');
     
-    let businessCardsFive = document.createElement('section')
+    let businessCardsFive = document.createElement('li')
     let businessNameFive = document.createElement('h3');
+    let businessCardTextContentFive = document.createElement('div')
+    businessCardTextContentFive.className = "directory-text-container"
     let businessOnwerFive = document.createElement('p');
     let streetAddressFive = document.createElement('p');
     let cityAndStateFive = document.createElement('p');
@@ -52,8 +62,10 @@ fetch(requestURL)
     let websiteFive = document.createElement('p');
     let businessLogoFive = document.createElement('img');
     
-    let businessCardsSix= document.createElement('section')
+    let businessCardsSix= document.createElement('li')
     let businessNameSix= document.createElement('h3');
+    let businessCardTextContentSix = document.createElement('div')
+    businessCardTextContentSix.className = "directory-text-container"
     let businessOnwerSix= document.createElement('p');
     let streetAddressSix= document.createElement('p');
     let cityAndStateSix= document.createElement('p');
@@ -61,8 +73,10 @@ fetch(requestURL)
     let websiteSix= document.createElement('p');
     let businessLogoSix= document.createElement('img');
     
-    let businessCardsSeven = document.createElement('section')
+    let businessCardsSeven = document.createElement('li')
     let businessNameSeven = document.createElement('h3');
+    let businessCardTextContentSeven = document.createElement('div')
+    businessCardTextContentSeven.className = "directory-text-container"
     let businessOnwerSeven = document.createElement('p');
     let streetAddressSeven = document.createElement('p');
     let cityAndStateSeven = document.createElement('p');
@@ -71,7 +85,7 @@ fetch(requestURL)
     let businessLogoSeven = document.createElement('img');
 
         
-            businessNameOne.textContent = business[1].name;
+    businessNameOne.textContent = business[1].name;
             businessOnwerOne.textContent = "Owner: " + business[1].owner;
             streetAddressOne.textContent = "Address: " + business[1].address;
             cityAndStateOne.textContent = business[1].cityAndState;
@@ -79,12 +93,13 @@ fetch(requestURL)
             websiteOne.textContent = "Website: " + business[1].website;
             businessLogoOne.src = business[1].logo;
             
-            businessCardsOne.appendChild(businessNameOne);
-            businessCardsOne.appendChild(businessOnwerOne);
-            businessCardsOne.appendChild(streetAddressOne);
-            businessCardsOne.appendChild(cityAndStateOne);
-            businessCardsOne.appendChild(phoneNumberOne);
-            businessCardsOne.appendChild(websiteOne);
+            businessCardTextContentOne.appendChild(businessNameOne);
+            businessCardTextContentOne.appendChild(businessOnwerOne);
+            businessCardTextContentOne.appendChild(streetAddressOne);
+            businessCardTextContentOne.appendChild(cityAndStateOne);
+            businessCardTextContentOne.appendChild(phoneNumberOne);
+            businessCardTextContentOne.appendChild(websiteOne);
+            businessCardsOne.appendChild(businessCardTextContentOne);
             businessCardsOne.appendChild(businessLogoOne);
             
             businessNameTwo.textContent = business[2].name;
@@ -95,12 +110,13 @@ fetch(requestURL)
             websiteTwo.textContent = "Website: " + business[2].website;
             businessLogoTwo.src = business[2].logo;
             
-            businessCardsTwo.appendChild(businessNameTwo);
-            businessCardsTwo.appendChild(businessOnwerTwo);
-            businessCardsTwo.appendChild(streetAddressTwo);
-            businessCardsTwo.appendChild(cityAndStateTwo);
-            businessCardsTwo.appendChild(phoneNumberTwo);
-            businessCardsTwo.appendChild(websiteTwo);
+            businessCardTextContentTwo.appendChild(businessNameTwo);
+            businessCardTextContentTwo.appendChild(businessOnwerTwo);
+            businessCardTextContentTwo.appendChild(streetAddressTwo);
+            businessCardTextContentTwo.appendChild(cityAndStateTwo);
+            businessCardTextContentTwo.appendChild(phoneNumberTwo);
+            businessCardTextContentTwo.appendChild(websiteTwo);
+            businessCardsTwo.appendChild(businessCardTextContentTwo);
             businessCardsTwo.appendChild(businessLogoTwo);
             
             businessNameThree.textContent = business[3].name;
@@ -111,12 +127,13 @@ fetch(requestURL)
             websiteThree.textContent = "Website: " + business[3].website;
             businessLogoThree.src = business[3].logo;
             
-            businessCardsThree.appendChild(businessNameThree);
-            businessCardsThree.appendChild(businessOnwerThree);
-            businessCardsThree.appendChild(streetAddressThree);
-            businessCardsThree.appendChild(cityAndStateThree);
-            businessCardsThree.appendChild(phoneNumberThree);
-            businessCardsThree.appendChild(websiteThree);
+            businessCardTextContentThree.appendChild(businessNameThree);
+            businessCardTextContentThree.appendChild(businessOnwerThree);
+            businessCardTextContentThree.appendChild(streetAddressThree);
+            businessCardTextContentThree.appendChild(cityAndStateThree);
+            businessCardTextContentThree.appendChild(phoneNumberThree);
+            businessCardTextContentThree.appendChild(websiteThree);
+            businessCardsThree.appendChild(businessCardTextContentThree);
             businessCardsThree.appendChild(businessLogoThree);
             
             
@@ -128,12 +145,13 @@ fetch(requestURL)
             websiteFour.textContent = "Website: " + business[4].website;
             businessLogoFour.src = business[4].logo;
             
-            businessCardsFour.appendChild(businessNameFour);
-            businessCardsFour.appendChild(businessOnwerFour);
-            businessCardsFour.appendChild(streetAddressFour);
-            businessCardsFour.appendChild(cityAndStateFour);
-            businessCardsFour.appendChild(phoneNumberFour);
-            businessCardsFour.appendChild(websiteFour);
+            businessCardTextContentFour.appendChild(businessNameFour);
+            businessCardTextContentFour.appendChild(businessOnwerFour);
+            businessCardTextContentFour.appendChild(streetAddressFour);
+            businessCardTextContentFour.appendChild(cityAndStateFour);
+            businessCardTextContentFour.appendChild(phoneNumberFour);
+            businessCardTextContentFour.appendChild(websiteFour);
+            businessCardsFour.appendChild(businessCardTextContentFour);
             businessCardsFour.appendChild(businessLogoFour);
             
             
@@ -145,12 +163,13 @@ fetch(requestURL)
             websiteFive.textContent = "Website: " + business[5].website;
             businessLogoFive.src = business[5].logo;
             
-            businessCardsFive.appendChild(businessNameFive);
-            businessCardsFive.appendChild(businessOnwerFive);
-            businessCardsFive.appendChild(streetAddressFive);
-            businessCardsFive.appendChild(cityAndStateFive);
-            businessCardsFive.appendChild(phoneNumberFive);
-            businessCardsFive.appendChild(websiteFive);
+            businessCardTextContentFive.appendChild(businessNameFive);
+            businessCardTextContentFive.appendChild(businessOnwerFive);
+            businessCardTextContentFive.appendChild(streetAddressFive);
+            businessCardTextContentFive.appendChild(cityAndStateFive);
+            businessCardTextContentFive.appendChild(phoneNumberFive);
+            businessCardTextContentFive.appendChild(websiteFive);
+            businessCardsFive.appendChild(businessCardTextContentFive);
             businessCardsFive.appendChild(businessLogoFive);
             
             
@@ -162,12 +181,13 @@ fetch(requestURL)
             websiteSix.textContent = "Website: " + business[6].website;
             businessLogoSix.src = business[6].logo;
             
-            businessCardsSix.appendChild(businessNameSix);
-            businessCardsSix.appendChild(businessOnwerSix);
-            businessCardsSix.appendChild(streetAddressSix);
-            businessCardsSix.appendChild(cityAndStateSix);
-            businessCardsSix.appendChild(phoneNumberSix);
-            businessCardsSix.appendChild(websiteSix);
+            businessCardTextContentSix.appendChild(businessNameSix);
+            businessCardTextContentSix.appendChild(businessOnwerSix);
+            businessCardTextContentSix.appendChild(streetAddressSix);
+            businessCardTextContentSix.appendChild(cityAndStateSix);
+            businessCardTextContentSix.appendChild(phoneNumberSix);
+            businessCardTextContentSix.appendChild(websiteSix);
+            businessCardsSix.appendChild(businessCardTextContentSix);
             businessCardsSix.appendChild(businessLogoSix);
             
             
@@ -179,23 +199,44 @@ fetch(requestURL)
             websiteSeven.textContent = "Website: " + business[7].website;
             businessLogoSeven.src = business[7].logo;
             
-            businessCardsSeven.appendChild(businessNameSeven);
-            businessCardsSeven.appendChild(businessOnwerSeven);
-            businessCardsSeven.appendChild(streetAddressSeven);
-            businessCardsSeven.appendChild(cityAndStateSeven);
-            businessCardsSeven.appendChild(phoneNumberSeven);
-            businessCardsSeven.appendChild(websiteSeven);
+            businessCardTextContentSeven.appendChild(businessNameSeven);
+            businessCardTextContentSeven.appendChild(businessOnwerSeven);
+            businessCardTextContentSeven.appendChild(streetAddressSeven);
+            businessCardTextContentSeven.appendChild(cityAndStateSeven);
+            businessCardTextContentSeven.appendChild(phoneNumberSeven);
+            businessCardTextContentSeven.appendChild(websiteSeven);
+            businessCardsSeven.appendChild(businessCardTextContentSeven);
             businessCardsSeven.appendChild(businessLogoSeven);
             
-            document.querySelector('div.localBusinesses').appendChild(businessCardsOne);
-            document.querySelector('div.localBusinesses').appendChild(businessCardsTwo);
-            document.querySelector('div.localBusinesses').appendChild(businessCardsSeven);
-            document.querySelector('div.localBusinesses').appendChild(businessCardsFour);
-            document.querySelector('div.localBusinesses').appendChild(businessCardsFive);
-            document.querySelector('div.localBusinesses').appendChild(businessCardsSix);
-            document.querySelector('div.localBusinesses').appendChild(businessCardsSeven);
+            document.querySelector('.localBusinesses').appendChild(businessCardsOne);
+            document.querySelector('.localBusinesses').appendChild(businessCardsTwo);
+            document.querySelector('.localBusinesses').appendChild(businessCardsSeven);
+            document.querySelector('.localBusinesses').appendChild(businessCardsFour);
+            document.querySelector('.localBusinesses').appendChild(businessCardsFive);
+            document.querySelector('.localBusinesses').appendChild(businessCardsSix);
+            document.querySelector('.localBusinesses').appendChild(businessCardsSeven);
             
         
     
     
 });
+const gridView = document.querySelector('.localBusinessss');
+const gridViewButton = document.querySelector('.directoryButton');
+
+function toggleView() {
+    
+    if (gridView.className === "list" && gridViewButton.className == "list") {
+        document.querySelector(".localBusinesses").classList.toggle("grid");
+        document.querySelector(".directoryButton").classList.toggle("grid");
+        delete gridView.className.list;
+        delete gridViewButton.className.list;
+
+    }
+    if (gridView.className === "grid" && gridViewButton.className == "grid") {
+        document.querySelector(".localBusinesses").classList.toggle("list");
+        document.querySelector(".directoryButton").classList.toggle("list");
+        delete gridView.className.list;
+        delete gridViewButton.className.list;
+
+    }
+}
